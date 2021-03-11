188566e3cdf0a2c465134bdff9af4c68c5a4bff2
nDox00 <80198134+nDox00@users.noreply.github.com>
2021-03-11 11:24:05 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,23 +1,21 @@
-requested_name = [input('Name : ').title()]
-req_password = [int(input('Password: '))]
+req_name = input('Name : ').title()
+req_password = int(input('Password: '))
 
-dic = {'John': 123, 'Mike': 32}
+dic = {'John':123,'Mike':32}
 auth = {'Admin': 142}
 
-
-for req_name, req_pass in zip(requested_name, req_password):
-    password = None
-    if req_name in dic:
-        print(f"Hello {req_name}")
-        password = dic[req_name]
-    elif req_name in auth:
-        print("Hello Admin, It's All Yours!")
-        password = auth[req_name]
+if req_name in dic.keys():
+    if req_password == dic.get(req_name):
+        print(f'Hello {req_name}')
+        print('Access Granted')
     else:
-        print("Wrong Username")
-        break
-
-    if req_pass == password:
-        print("Access Granted.!")
+        print(f'Sorry {req_name}, Access Denied')
+        
+elif req_name in auth.keys():
+    if req_password == auth.get(req_name):
+        print(f'Hello {req_name} take control')
+        print('Access Granted')
     else:
-        print("Access Denied.!")
+        print(f'Sorry {req_name}, Access Denied')
+else:
+    print('Wrong Username')
